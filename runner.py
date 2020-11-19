a93b9dc1b19ce2268e00501e87e20670a24dbfbd
vasu77df <vasu3797@gmail.com>
2020-11-19 17:45:09 -0600

    finished comments

--- runner.py
+++ runner.py
@@ -1,13 +1,14 @@
+# This is the programs that is used to trigger various procudure in the Intial access of the UE.
 import gNB
 import UE
-def Reg_trigger():
+def Reg_trigger(): #function to trigger the UE Registration procedure
     print("----------------Starting the UE Registration Procedure---------------")
     operation_trigger = {
         'msg_type': "REGISTRATION REQUEST"
     }
     UE.UL_SENDER(operation_trigger)
 
-def PDU_trigger():
+def PDU_trigger(): #function to trigger the PDU Session Establishment Procedure
     print("-----------------Starting PDU Session Establishement----------------")
     payload = {
         'msg_type': "PDU Session Establishment Request",
@@ -19,7 +20,7 @@ def PDU_trigger():
 
 
 if __name__ == "__main__":
-    operation_trigger = {
+    operation_trigger = { # payload sent to send the sync signal from gNB to UE
         'msg_type': 'PSS/SSS'
     }
     gNB.gNB_SENDER(operation_trigger)
